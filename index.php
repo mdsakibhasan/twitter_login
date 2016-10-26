009918eabeb3207808b0225851c28e8b6411dedb
lit-sakibhasan <lit.sakibhasan@gmail.com>
2016-10-26 18:31:59 +0600

    here is the twitter page layout done

---
+++ index.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Twitter. it's with happening</title>
+	<link rel="shortcut icon" href="images/twitter.png" type="image/x-icon">
+    <link rel="icon" href="images/twitter.png" type="image/x-icon">
+	<link rel="stylesheet" href="css/bootstrap-theme.css">
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/font-awesome.css">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<!-- header area start -->
+	<header>
+		<div class="header">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6">
+						<div class="logo-left text-left">
+							<a href="#"><i class="fa fa-twitter">Home</i></a>
+							<a href="#">About</a>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="language-area text-right">
+							<select name="language English" id="">
+								<option value="english">Language English</option>
+								<option value="english">bangla</option>
+								<option value="bangla">bangla</option>
+								<option value="bangla">bangla</option>
+								<option value="bangla">bangla</option>
+								<option value="bangla">bangla</option>
+								<option value="bangla">bangla</option>
+							</select>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+	<!-- header area end -->
+
+	<!-- main area start -->
+	<div class="main-area">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-10 col-md-offset-1">
+					<div class="row">
+						<div class="col-md-6">
+							<div class="main-left-site">
+								<h2>Welcome to Twitter.</h2>
+								<p>Connect with your friends — and other fascinating people. Get in-the-moment updates on the things that interest you. And watch events unfold, in real time, from every angle.</p>
+								<p>트위터 포토 기능 업제이트 기념 봄 사진 포스팅. 남쪽엔 봄 꽃이 한창. 매화, 산수유, 동백.</p>
+								<a>Tweet and photo by @JYHeffect </a><br>
+								<a>12:40 PM - 8 Sep 2014</a>
+							</div>
+						</div>
+						<div class="col-md-6">
+							<div class="main-right-site">
+								<div class="login-area">
+									<form action="" method="POST">
+										<input type="email" name="email" placeholder="phone email or username">
+										<input type="password" name="password" placeholder="password">
+										<input type="submit" name="login" value="Log in">
+										<input type="checkbox" name="box" value="">Remember me
+										<a href="#">Forgot Password?</a>
+									</form>
+								</div>
+								<div class="signup-area">
+									<h3>New To Twitter? Sign up</h3>
+									<form action="" method="POST">
+										<input type="text" name="Full-name" placeholder="Full name">
+										<input type="email" name="email" placeholder="Email">
+										<input type="password" name="" placeholder="Password">
+										<input type="submit" name="signup" value="Sign up tor Twitter">
+									</form>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- main area end -->
+
+	<!-- footer area start -->
+	<footer>
+		<div class="footer">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8 col-md-offset-2">
+						<div class="footer-menu">
+							<ul class="list-inline">
+								<li>About</li>                
+								<li>Help</li>
+								<li>Blog</li>
+								<li>Status</li>
+								<li>Jobs</li>
+								<li>Terms</li>
+								<li>Privacy</li>
+								<li>Cookies</li>
+								<li>Ads</li>
+								<li>info</li>
+								<li>Brand</li>
+								<li>Advertise</li>
+								<li>Businesses</li>
+								<li>Media</li>
+								<li>Developers</li>
+								<li>Directory</li>
+								<li>© 2016 Twitter</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+	<!-- footer area end -->
+
+
+
+
+
+
+
+
+	<script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/custom.js"></script>
+</body>
+</html>
